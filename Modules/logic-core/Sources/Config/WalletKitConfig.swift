8af27544c7402b1d7534e8975ea5f525c8df8bfa
DusanNDigDir <dusan.nikolic@digdir.no>
2025-09-22 15:20:12 +0200

    Certificate and issuer url

--- Modules/logic-core/Sources/Config/WalletKitConfig.swift
+++ Modules/logic-core/Sources/Config/WalletKitConfig.swift
@@ -163,13 +163,7 @@ struct WalletKitConfigImpl: WalletKitConfig {
 
   var readerConfig: ReaderConfig {
     let certificates = [
-      "pidissuerca02_cz",
-      "pidissuerca02_ee",
-      "pidissuerca02_eu",
-      "pidissuerca02_lu",
-      "pidissuerca02_nl",
-      "pidissuerca02_pt",
-      "pidissuerca02_ut"
+        "eidas2sandkasse_net_access_CA"
     ]
     let certsData: [Data] = certificates.compactMap {
       Data(name: $0, ext: "der")
